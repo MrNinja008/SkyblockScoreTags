fd29facf829b89a65a83744c93f9f07a2368b6ed
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-07-16 10:27:12 +0530

    Update TagResolveListener.php
    Why

--- src/MrNinja008/sbscore/listeners/TagResolveListener.php
+++ src/MrNinja008/sbscore/listeners/TagResolveListener.php
@@ -1,55 +1,80 @@
 <?php
+
 declare(strict_types = 1);
 
 namespace MrNinja008\sbscore\listeners;
 
 use Ifera\ScoreHud\event\TagsResolveEvent;
-use MrNinja008\sbscore\Main;
+
+use MrNinja008\sbcore\Main;
+
 use pocketmine\event\Listener;
+
 use function count;
+
 use function explode;
+
 use function strval;
 
 class TagResolveListener implements Listener{
 
-	/** @var Main */
-	private $plugin;
+	/** @var Main */	private $plugin;
 
 	public function __construct(Main $plugin){
+
 		$this->plugin = $plugin;
+
 	}
 
 	public function onTagResolve(TagsResolveEvent $event){
+
 		$tag = $event->getTag();
-		$tags = explode('.', $tag->getName(), 5);
+
+		$tags = explode('.', $tag->getName(), 4);
+
 		$value = "";
 
-		if($tags[0] !== 'is' || count($tags) < 5){
+		if($tags[0] !== 'skyblock' || count($tags) < 4){
+
 			return;
+
 		}
 
 		switch($tags[1]){
+
 			case "state":
-				$value = $this->isLocked($event->getPlayer());
+
+				$value = $this->plugin->getIsleState($event->getPlayer());
+
 			break;
 
 			case "blocks":
-				$value = $this->getBlocksBuilt($event->getPlayer());
+
+				$value = $this->plugin->getIsleBlocks($event->getPlayer());
+
 			break;
+
 			
+
 			case "size":
-			  $value = $this->getCategory($event->getPlayer());
+
+			  $value = $this->plugin->getIsleSize($event->getPlayer());
+
 			break;
+
 			
-			case "members":
-			  $value = $this->getMembers($event->getPlayer());
-			break;
-			
+
 			case "rank":
-			  $value = $this->getRank($event->getPlayer());
+
+			  $value = $this->plugin->getIsleRank($event->getPlayer());
+
 			break;
+
 		}
 
 		$tag->setValue(strval($value));
+
 	}
+
 }
+			
